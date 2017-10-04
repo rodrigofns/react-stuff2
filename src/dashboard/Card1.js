0b4903c75f5cb3faff9b889b123fe3688c4f03ee
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2017-10-04 14:19:55 -0300

    Aligning 1st dashboard card.

--- src/dashboard/Card1.js
+++ src/dashboard/Card1.js
@@ -4,6 +4,7 @@ import {subscribe} from 'react-app-state';
 import SelecionaVisao from './seleciona-visao/SelecionaVisao';
 import Internas from './visoes/Internas';
 import visaoState from './seleciona-visao/visaoState';
+import './Card1.scss';
 
 @subscribe(visaoState)
 export default class Card1 extends React.Component {
--- src/dashboard/visoes/Internas.js
+++ src/dashboard/visoes/Internas.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Internas.scss';
 
 export default class Internas extends React.Component {
 	render() {
 		return (
-			<div style={{marginTop:'100px'}}>Internas</div>
+			<div id="VisaoInternas">
+				<div className="navegador">Navega</div>
+				<div className="mapa">Mapa</div>
+			</div>
 		);
 	}
 }

9be824d849da5687ad7a36da4e626cd728820b30
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2017-10-17 11:18:50 -0200

    Expired session handling.

--- src/_util/httpSgu.js
+++ src/_util/httpSgu.js
@@ -4,6 +4,7 @@ import {globalMsgStore} from './globalMsgStore';
 
 const API_URL = process.env.REACT_APP_API_URL; // veja arquivo ".env" na raiz do projeto
 const TOKEN_HEADER_NAME = 'set-token';
+const EXPIRED_HEADER_NAME = 'sessao-expirada';
 const AUTH_HEADER_NAME = 'Authorization';
 const AUTH_HEADER_VALUE_PREFIX = 'Token ';
 
@@ -28,16 +29,21 @@ class HttpSgu {
 		let headers = { 'Content-Type': 'application/json' };
 		this._addToken(headers);
 
-		return axios({
-			method: method,
-			url: API_URL + path,
-			headers: headers,
-			data: body
-		}).then(resp => {
-			this._saveToken(resp); // sempre salva o token JWT a cada requisição
-			return resp.data;
-		}).catch(err => {
-			this._handleError(err);
+		return new Promise((resolve, reject) => {
+			return axios({
+				method: method,
+				url: API_URL + path,
+				headers: headers,
+				data: body
+			}).then(response => {
+				if (path === '/auth') {
+					globalMsgStore.clear(); // ao logar, todas as mensagens de erro são limpas
+				}
+				if (!this._handleExpired(response)) {
+					this._saveToken(response); // sempre salva o token JWT a cada requisição
+					resolve(response.data);
+				}
+			}).catch(err => reject(this._handleError(err)));
 		});
 	}
 
@@ -53,17 +59,25 @@ class HttpSgu {
 		authStore.saveToken(token);
 	}
 
+	_handleExpired(response) {
+		if (response.headers[EXPIRED_HEADER_NAME]) {
+			globalMsgStore.add('Sua sessão expirou.');
+			authStore.removeToken(); // vai para a página de login imediatamente
+			return true;
+		}
+		return false;
+	}
+
 	_handleError(err) {
-		// authStore.removeToken();
 		if (err.response) { // servidor respondeu com um status de erro
-			globalMsgStore.msgs.push(`${err.response.status}: ${err.response.statusText} - ${err.response.data}`);
+			globalMsgStore.add(`${err.response.status}: ${err.response.statusText} - ${err.response.data}`);
 			console.error('SGU', err.response);
-			throw err;
 		} else if (err.request) { // nenhuma resposta recebida do servidor
 			console.error('SGU', err.response);
 		} else { // outro erro
 			console.error('SGU', err);
 		}
+		return err;
 	}
 }
 

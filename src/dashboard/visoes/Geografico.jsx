136a234c49d1faf4d00649781cbc34bd810db5d5
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2017-10-20 08:47:47 -0200

    Custom React Scripts to v0.2.1.

--- src/dashboard/visoes/Geografico.jsx
+++ src/dashboard/visoes/Geografico.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import httpDashboard from '../httpDashboard';
+import Visao from '../visao/Visao';
+
 export default class Geografico extends React.PureComponent {
+	getOrgaos() {
+		return httpDashboard.getOrgaosFederais(); // mudar !!!
+	}
+
+	getAbrangencia(idOrgaoFed) {
+		return httpDashboard.getAbrangenciaFederal(idOrgaoFed); // mudar !!!
+	}
+
 	render() {
 		return (
-			<div>Geografico</div>
+			<Visao
+				idConjuntoInicial="regioes"
+				nomeConjuntoInicial="Regiões"
+				onGetOrgaos={this.getOrgaos}
+				onGetAbrangencia={this.getAbrangencia}/>
 		);
 	}
 }
